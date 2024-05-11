2aa27c3fb283ff442f9b496842c320c1341f3e8b
JEduardo001 <125694974+JEduardo001@users.noreply.github.com>
2024-05-11 15:37:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 //const db = require('./dbFirestore'); // Importa la conexión a la base de datos
 const admin = require('firebase-admin');
-
 var serviceAccount = require("./refugio-animal-92181-firebase-adminsdk-h8a3q-d18c72f487.json");
+
+//firestorage
+const multer = require('multer');
+const { Storage } = require('@google-cloud/storage');
+
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -12,8 +19,11 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-
-
+const upload = multer({ dest: 'uploads/' });
+// Crea una instancia de Storage de Google Cloud
+const storage = new Storage();
+// Obtiene una referencia al bucket de Firebase Storage
+const bucket = storage.bucket(admin.storage().bucket().name);
 
 
 // Ruta de ejemplo
@@ -58,5 +68,67 @@ app.get('/datos', async (req, res) => {
   } catch (error) {
     console.error('Error al obtener datos:', error);
     res.status(500).json({ error: 'Ocurrió un error al obtener datos.' });
+  }
+});
+
+
+//subir la imagen a fire storage
+app.post('/subirImagen', upload.single('image'), async (req, res) => {
+  try {
+    // Verifica si se subió un archivo
+    if (!req.file) {
+      return res.status(400).json({ error: 'No se proporcionó ningún archivo' });
+    }
+
+    // Guarda el archivo en Firebase Storage
+    const file = req.file;
+    const nombre = req.body.nombre;
+    const raza = req.body.raza;
+    const edad = req.body.edad;
+
+    const blob = bucket.file(file.originalname);
+
+    // Crea un stream de escritura para cargar el archivo
+    const blobStream = blob.createWriteStream({
+      metadata: {
+        contentType: file.mimetype
+      }
+    });
+
+    // Maneja los eventos del stream
+    blobStream.on('error', err => {
+      console.error('Error al cargar el archivo:', err);
+      res.status(500).json({ error: 'Error al cargar el archivo' });
+    });
+
+
+    querySnapshot = await FirebaseFirestore.instance.collection('gatos').get();
+    var cantidadGatos = querySnapshot.size;
+    cantidadGatos++;
+  
+    blobStream.on('finish', async () => {
+      // Una vez que la carga del archivo se ha completado, genera la URL de descarga
+      const imageUrl = await blob.getSignedUrl({ action: 'read', expires: '01-01-2100' });
+      
+      // Guarda la URL en Firestore u otra base de datos si es necesario
+      await firestore.collection('gatos').add({ 
+          'edad': edad,
+          'id': cantidadGatos,
+          'imagen': imageUrl,
+          'nombre': nombre,
+          'raza': raza
+
+        }
+      );
+
+      // Responde con la URL de la imagen
+      res.json({ imageUrl });
+    });
+
+    // Escribe el archivo en el stream
+    blobStream.end(file.buffer);
+  } catch (error) {
+    console.error('Error al subir la imagen:', error);
+    res.status(500).json({ error: 'Error al subir la imagen' });
   }
 });
