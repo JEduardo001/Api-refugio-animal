c8c7ce8dc6e89a7b3e7c2cc71320cc84e1ce6004
JEduardo001 <125694974+JEduardo001@users.noreply.github.com>
2024-05-12 10:01:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -70,11 +70,11 @@ app.get('/datos', async (req, res) => {
 });
 
 
-app.post('/subirImagen',  (req, res) => {
+app.post('/subirImagen', async (req, res) => {
   // Aquí puedes acceder a los parámetros enviados en la solicitud
   
   //const imageData = req.image; // Aquí obtienes la imagen
-  const edad = req.body.edad;
+  const edad = await req.body.edad;
   //const raza = req.body.raza;
   const nombre = req.body.nombre;
   console.log('el valor de edad es: ',edad);
