229db257b11993ff19ae529da6e20b62fe0b23f6
JEduardo001 <125694974+JEduardo001@users.noreply.github.com>
2024-05-28 16:25:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -86,49 +86,134 @@ app.get('/verGatos', async (req, res) => {
 
 });
 
-app.post('/subirImagen', (req, res) => {
+app.get('/subirImagen', (req, res) => {
+  const collectionRef = db.collection('perros');
+  // Obtén el número total de documentos en la colección
+  collectionRef.get()
+    .then((querySnapshot) => {
+      let numberOfDocuments = querySnapshot.size;
+      console.log('Número total de documentos:', numberOfDocuments );
+      numberOfDocuments = numberOfDocuments+1;
+      console.log('  el id del nuevo sera'+numberOfDocuments);
+
+    })
+    .catch((error) => {
+      console.error('Error al obtener la colección para obtener su ID:', error);
+  });
+  res.send('listo');
+
+  
+    
+});
+
+function getId(coleccion){
+
+  const collectionRef = db.collection(coleccion);
+
+
+  collectionRef.get()
+    .then((querySnapshot) => {
+      let numberOfDocuments = querySnapshot.size;
+      numberOfDocuments = numberOfDocuments+1;
+
+    })
+    .catch((error) => {
+      console.error('Error al obtener la colección para obtener su ID:', error);
+  });
+
+  return numberOfDocuments;
+}
+
+app.post('/SubirMascota', (req, res) => {
   // Aquí puedes acceder a los parámetros enviados en la solicitud
   const nombre =  req.body.nombre;
   //const imageData = req.file; // Aquí obtienes la imagen
   const edad = req.body.edad;
   const raza = req.body.raza;
   const imagen = req.body.imagen;
-  
-  const numberOfDocuments = null;
-  const collectionRef = db.collection('gatos');
-  // Obtén el número total de documentos en la colección
-  /*collectionRef.get()
-    .then((querySnapshot) => {
-      numberOfDocuments = querySnapshot.size;
-      console.log('Número total de documentos:', numberOfDocuments);
-      numberOfDocuments+1;
-    })
-    .catch((error) => {
-      console.error('Error al obtener la colección para obtener su ID:', error);
-  });*/
+  const fechaIngreso = req.body.fecha;
+  const tipoMascota = req.body.tipoMascota;
+  const destinoMascota = req.body.destinoMascota;
+  var  coleccionDatos;
+  var data2;
 
-  // Sube datos a Firestore
+  switch(tipoMascota){
+    case 'gatos':
+      if(destinoMascota=='adoptar'){
+        coleccionDatos='gatosAdopcion'
+        data2 = {
+          'idGato': numberOfDocuments,
+          'fechaIngreso': fechaIngreso
+      
+        };
+      }else{
+        coleccionDatos='gatosPerdidos'
+        data2 = {
+          'idGato': numberOfDocuments,
+          'fechaPerdido': fechaIngreso
+      
+        };
+      }
+      
+    break;
+    case 'perros':
+      if(destinoMascota=='adoptar'){
+        coleccionDatos='perrosAdopcion'
+        data2 = {
+          'idPerro': numberOfDocuments,
+          'fechaIngreso': fechaIngreso
+      
+        };
+      }else{
+        coleccionDatos='perrosPerdidos'
+        data2 = {
+          'idPerro': numberOfDocuments,
+          'fechaPerdido': fechaIngreso
+      
+        };
+      }
+    break;
+  }
+  
+  const collectionRef1 = db.collection(tipoMascota);
+  const collectionRef2 = db.collection(coleccionDatos);
+
+  const numberOfDocuments = getId(tipoMascota);
+
+
+  //Subimos los datos a la coleccion perros
   const data = {
     'nombre': nombre,
     'edad': edad,
     'raza': raza,
     'imagen': imagen,
-    'id': 2
+    'id': numberOfDocuments,
   };
 
-  collectionRef.add(data)
+  collectionRef1.add(data)
     .then((docRef) => {
-      console.log('Documento agregado con ID:', 1);
+      console.log('Documento agregado con ID:', numberOfDocuments);
     })
     .catch((error) => {
-      console.error('Error al agregar documento:', error);
+      console.error('Error al agregar documento :', error);
     });
-    // Envía una respuesta al cliente
-    res.send('Imagen recibida correctamente');
+    res.send('agregado correctamanete');
+
+  //Subimos los datos a la coleccion perrosAdopcion
+  
+
+  collectionRef2.add(data2)
+    .then((docRef) => {
+      console.log('Documento agregado con ID:', numberOfDocuments);
+    })
+    .catch((error) => {
+      console.error('Error al agregar documento :', error);
+    });
+    res.send('mascota ingresado correctamente');
 });
 
-//-------------- verGatosVerTodos
-app.get('/verGatosVerTodos', async (req, res) => {
+//-------------- verGatosTodos
+app.get('/verGatosTodos', async (req, res) => {
   try {
     const datos = [];
     const snapshot = await db.collection('gatos').get();
@@ -348,70 +433,4 @@ app.get('/VerPerrosVerPerdidos', async (req, res) => {
 
 });
 
-/*app.get('/test', async (req, res) => {
-  
-  const collectionRef = db.collection('gatos');
 
- 
-});*/
-
-//subir la imagen a fire storage
-/*app.post('/subirImagen', upload.single('image'), async (req, res) => {
-  try {
-    // Verifica si se subió un archivo
-    if (!req.file) {
-      return res.status(400).json({ error: 'No se proporcionó ningún archivo' });
-    }
-
-    // Guarda el archivo en Firebase Storage
-    const file = req.file;
-    const nombre = req.body.nombre;
-    const raza = req.body.raza;
-    const edad = req.body.edad;
-
-    const blob = bucket.file(file.originalname);
-
-    // Crea un stream de escritura para cargar el archivo
-    const blobStream = blob.createWriteStream({
-      metadata: {
-        contentType: file.mimetype
-      }
-    });
-
-    // Maneja los eventos del stream
-    blobStream.on('error', err => {
-      console.error('Error al cargar el archivo:', err);
-      res.status(500).json({ error: 'Error al cargar el archivo' });
-    });
-
-
-    querySnapshot = await FirebaseFirestore.instance.collection('gatos').get();
-    var cantidadGatos = querySnapshot.size;
-    cantidadGatos++;
-  
-    blobStream.on('finish', async () => {
-      // Una vez que la carga del archivo se ha completado, genera la URL de descarga
-      const imageUrl = await blob.getSignedUrl({ action: 'read', expires: '01-01-2100' });
-      
-      // Guarda la URL en Firestore u otra base de datos si es necesario
-      await firestore.collection('gatos').add({ 
-          'edad': edad,
-          'id': cantidadGatos,
-          'imagen': imageUrl,
-          'nombre': nombre,
-          'raza': raza
-
-        }
-      );
-
-      // Responde con la URL de la imagen
-      res.json({ imageUrl });
-    });
-
-    // Escribe el archivo en el stream
-    blobStream.end(file.buffer);
-  } catch (error) {
-    console.error('Error al subir la imagen:', error);
-    res.status(500).json({ error: 'Error al subir la imagen' });
-  }
-});*/
